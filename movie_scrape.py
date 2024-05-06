fc5d771d1808c3132db81bd5c56ac9db6411b0af
KINGSLEY ESISI <138278444+kingsleyesisi@users.noreply.github.com>
2024-05-06 20:45:06 +0100

    Add files via upload

---
+++ movie_scrape.py
@@ -0,0 +1,26 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+latest_movies = []
+print("Your Movie list is on it's way")
+print('What Would you like to save it with?')
+file_name = input('>')
+
+for no in range(1,2):
+    url = f"https://www.rottentomatoes.com/browse/movies_in_theaters/sort:newest?page={no}"
+    response = requests.get(url)
+    response = response.content
+    soup = BeautifulSoup(response, 'html.parser')
+    container = soup.find_all('div', class_ = 'flex-container')
+
+    for movie in container:
+        Title = movie.find('span', class_ ='p--small').text.strip()
+        release = movie.find('span', class_ ='smaller').text.strip()
+        latest_movies.append([Title, release])
+data = pd.DataFrame(latest_movies, columns=['MOVIE NAME', 'DATE RELEASE'])
+
+data.to_csv(f'{file_name}.csv')
+print('Movie List Saved')
+print("""Thanks, 
+and Expect more from THE KINGS WORLD""")
